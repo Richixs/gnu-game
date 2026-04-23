d07ff8bb4ec5972fb6e862a1038caeb141966460
Ricardo Ramirez <ricardo@richixs.dev>
2026-04-23 12:28:42 -0400

    feat(world): add background paralaxlayer

--- levels/world/scripts/track/scrolling_background.gd
+++ levels/world/scripts/track/scrolling_background.gd
@@ -2,5 +2,5 @@ extends ParallaxBackground
 
 @export var scroll_speed: float = 250.0
 
-func _process(delta):
+func _physics_process(delta: float) -> void:
 	scroll_base_offset.x -= scroll_speed * delta
